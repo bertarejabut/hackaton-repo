af73c17d9e46bbd171a51205a1d9db54dee2b031
Berta Reja Butvilaite <berta.butvilaite@devbridge.com>
2023-07-22 19:28:15 +0300

    increase test execution timeout

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -149,7 +149,7 @@ export const config: Options.Testrunner = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 600000
     },
     //
     // =====
